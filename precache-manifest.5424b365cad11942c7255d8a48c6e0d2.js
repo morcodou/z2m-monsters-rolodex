a740c60adc534d8df981af4b399ec410d1ea3a7a
Mor Codou GUEYE <morcodou@hotmail.com>
2019-10-13 03:39:48 -0400

    Updates

---
+++ precache-manifest.5424b365cad11942c7255d8a48c6e0d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc93e2f416ed51cfcd66addd80acb6f",
+    "url": "/z2m-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0fa4f163c28ea48a8379",
+    "url": "/z2m-monsters-rolodex/static/css/main.c3f3cd4e.chunk.css"
+  },
+  {
+    "revision": "a7ffd671cb21d8ff7fb4",
+    "url": "/z2m-monsters-rolodex/static/js/2.fe5198a4.chunk.js"
+  },
+  {
+    "revision": "0fa4f163c28ea48a8379",
+    "url": "/z2m-monsters-rolodex/static/js/main.7f85fc1e.chunk.js"
+  },
+  {
+    "revision": "c3685d4825f04a8be546",
+    "url": "/z2m-monsters-rolodex/static/js/runtime-main.4b2620cc.js"
+  }
+]);
